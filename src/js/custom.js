749dc2362a3c723c1edbc67e0bc21f35a0f6b691
Martin Schoen <schoenm@cip.ifi.lmu.de>
2018-07-04 12:58:57 +0200

    Refine content and add some invisible glamour

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,1 +1,20 @@
-console.log('Custom JS is empty, remove it')
+let string = `
+
+██╗  ██╗███████╗██╗   ██╗    ████████╗██╗  ██╗███████╗██████╗ ███████╗   
+██║  ██║██╔════╝╚██╗ ██╔╝    ╚══██╔══╝██║  ██║██╔════╝██╔══██╗██╔════╝   
+███████║█████╗   ╚████╔╝        ██║   ███████║█████╗  ██████╔╝█████╗     
+██╔══██║██╔══╝    ╚██╔╝         ██║   ██╔══██║██╔══╝  ██╔══██╗██╔══╝     
+██║  ██║███████╗   ██║          ██║   ██║  ██║███████╗██║  ██║███████╗▄█╗
+╚═╝  ╚═╝╚══════╝   ╚═╝          ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝
+                                                                         
+`
+
+console.log(string)
+console.log(`
+    fellow developer. I hope you are enjoying this page. If so, 
+    feel free to leave me a message. If not, leave me a message as well.
+    In any case you want to tell me something, here is my email adress:
+    
+    martingeorg.schoen@gmail.com
+    
+    Thanks!`)
