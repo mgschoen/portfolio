160944bd40ed1f4623f23c2d240817a1f41c4699
Martin Schoen <schoenm@cip.ifi.lmu.de>
2020-09-21 22:10:52 +0200

    Refactored dimension properties

--- src/js/_code-jumbotron/index.js
+++ src/js/_code-jumbotron/index.js
@@ -13,19 +13,24 @@ export default class CodeJumbotron {
         // define general properties
         this.backgroundColor = 'rgb(233, 236, 239)';
         this.canvas = document.getElementById('code-jumbotron');
-        this.config = { width: this.canvas.offsetWidth, height: this.canvas.offsetHeight };
+        this.width = this.canvas.offsetWidth;
+        this.height = this.canvas.offsetHeight;
         this.direction = .5;
 
         // init scene
         this.scene = new Scene();
         this.scene.background = new Color(this.backgroundColor);
-        this.camera = new PerspectiveCamera(75, this.config.width / this.config.height, 0.1, 1000);
+        this.camera = new PerspectiveCamera(75, this.aspect, 0.1, 1000);
         this.camera.position.set(0, -80, 200);
         this.renderer = new WebGLRenderer({ canvas: this.canvas });
-        this.renderer.setSize(this.config.width, this.config.height);
+        this.renderer.setSize(this.width, this.height);
 
         // init slide
-        this.slide = new CodeSlide({ background: this.backgroundColor, ...this.config });
+        this.slide = new CodeSlide({
+            background: this.backgroundColor,
+            width: this.width,
+            height: this.height
+        });
         this.scene.add(this.slide.mesh);
 
         this.animate();
@@ -41,5 +46,42 @@ export default class CodeJumbotron {
         this.camera.position.y += this.direction;
         this.renderer.render(this.scene, this.camera);
         window.requestAnimationFrame(this.animate.bind(this));
+    }
+
+    get aspect() {
+        if (!this._dimensions) {
+            this._dimensions = { width: 1, height: 1, aspect: 1 };
+        }
+        return this._dimensions.aspect;
+    }
+
+    get width() {
+        if (!this._dimensions) {
+            this._dimensions = { width: 1, height: 1, aspect: 1 };
+        }
+        return this._dimensions.width;
+    }
+
+    get height() {
+        if (!this._dimensions) {
+            this._dimensions = { width: 1, height: 1, aspect: 1 };
+        }
+        return this._dimensions.height;
+    }
+
+    set width(value) {
+        if (!this._dimensions) {
+            this._dimensions = { width: 1, height: 1, aspect: 1 };
+        }
+        this._dimensions.width = value;
+        this._dimensions.aspect = this._dimensions.width / this._dimensions.height;
+    }
+
+    set height(value) {
+        if (!this._dimensions) {
+            this._dimensions = { width: 1, height: 1, aspect: 1 };
+        }
+        this._dimensions.height = value;
+        this._dimensions.aspect = this._dimensions.width / this._dimensions.height;
     }
 };
